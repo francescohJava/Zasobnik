4dc3ad0e69dfb309ffe77751d1d8698a1ad74990
Frantisek <Frantisek@192.168.0.164>
2017-12-13 19:06:50 +0100

    oprava

--- Zasobnik/src/VytZasobnika.java
+++ Zasobnik/src/VytZasobnika.java
@@ -5,6 +5,20 @@ public class VytZasobnika
 	public static void main(String[] args)
 	{
 		Zasobnik maleCisla = new Zasobnik(10);
+		
+		try
+		{
+			maleCisla.vloz(5);
+			System.out.println(maleCisla.vyjmi());
+		}
+		catch(VyjimkaPlnyZasobnik vyj)
+		{
+			System.out.println(vyj);
+		}
+		catch(VyjimkaPrazdnyZasobnik vyj)
+		{
+			System.out.println(vyj);
+		}
 
 	}
 
